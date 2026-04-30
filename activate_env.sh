060438ff881a19fb1f9a250b8547d1d46fd09d9e
Anastasiia Boiko <nastya.boiko@scrumlaunch.com>
2026-04-30 17:28:02 +0200

    Remove activate_env.sh script as it is no longer needed for virtual environment activation

--- activate_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Activate the virtual environment for this project
-source .venv/bin/activate
-echo "Virtual environment activated. Python version:"
-python --version
-echo "Available packages:"
-pip list | grep -E "(anthropic|dotenv|jupyter|ipykernel)"
